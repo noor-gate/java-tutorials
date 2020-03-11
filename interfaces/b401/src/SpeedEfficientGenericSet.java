156edfdcaa79b959ac4b2fd733663d2f19851804
Noor Sawhney <noor@dyn3159-250.wlan.ic.ac.uk>
2020-03-11 15:57:11 +0000

    added b401

---
+++ interfaces/b401/src/SpeedEfficientGenericSet.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class SpeedEfficientGenericSet<T> implements GenericSet<T> {
+
+    private Set<T> integerSet;
+
+    public SpeedEfficientGenericSet() {
+        integerSet = new HashSet<>();
+    }
+
+    @Override
+    public void add(T x) {
+        integerSet.add(x);
+    }
+
+    @Override
+    public boolean remove(T x) {
+        return integerSet.remove(x);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return integerSet.isEmpty();
+    }
+
+    @Override
+    public boolean contains(T x) {
+        return integerSet.contains(x);
+    }
+}
